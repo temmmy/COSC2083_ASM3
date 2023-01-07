ef852dec8bb1f346d99fb943ab405a03579c93a9
Mr. Nikipon <temmmyofficial@gmail.com>
2023-01-07 15:07:57 +0700

    prototype v1.0

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "@fontsource/nunito";
+    import "../app.css";
+</script>
+
+<style>
+    :global(body) {
+      font-family: "Nunito", sans-serif;
+    }
+</style>
+<slot />
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,161 @@
+<script>
+</script>
+<head>
+    <title>SIUUUUUUUU</title>
+</head>
+<body class="bg-body text-white pb-12 scrollbar">
+    <!-- Home Page -->
+    <header class="py-6">
+        <div class="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+            <div class="text-2xl font-bold">Nikitech Inc.</div>
+            <div>
+                <div class="hidden md:flex space-x-12 items-center">
+                    <a href="#" class="text-selected-text">Home</a>
+                    <a href="#" class="">Our Team</a>
+                    <a href="#">Tools</a>
+                    <a href="#">Project Description</a>
+                    <a href="#">Skills and Jobs</a>
+                    <a href="#">Project Outcomes</a>
+                </div>
+            </div>
+            <div class="md:hidden">
+                <svg width="26" height="18" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z" fill="white"/></svg>
+            </div>
+        </div>
+    </header>
+    <div class="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+        <div class="flex flex-wrap md:flex-nowrap">
+            <nav class="inline-block mt-32 lg:mr-24 lg:w-4 fixed left-percentage hidden xl:block">
+                <div class="absolute left-50 transform -translate-x-1/2 space-y-6 mt-37">
+                    <a href="#" class="nav-dot selected-circle block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Home</span>
+                    </a>
+                    <a href="#" class="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black  px-2 py-1 rounded-md ml-10 opacity-0">Our_Team</span>
+                    </a>
+                    <a href="#" class="nav-dot  block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Tools</span>
+                    </a>
+                    <a href="#" class="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Project_Description</span>
+                    </a>
+                    <a href="#" class="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Skills_and_Jobs</span>
+                    </a>
+                    <a href="#" class="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
+                        <span class="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Project_Outcomes</span>
+                    </a>
+                </div>
+            </nav>
+            <div class="flex flex-wrap lg:ml-20 justify-center md:justify-start max-w-xl mt-0 md:my-36">
+                <h1 class="font-bold text-5xl md:text-6xl lg:text-8xl text-center md:text-left">Helping<br> businesses to grow.</h1>
+                <div class="w-full flex justify-center md:justify-start">
+                    <button class="px-8 py-4 rounded-full bg-theme text-white font-bold mt-12 flex items-center space-x-3">
+                        <div>
+                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.4375 11C14.4375 11.9117 14.0753 12.786 13.4307 13.4307C12.786 14.0753 11.9117 14.4375 11 14.4375C10.0883 14.4375 9.21398 14.0753 8.56932 13.4307C7.92466 12.786 7.5625 11.9117 7.5625 11C7.5625 10.0883 7.92466 9.21398 8.56932 8.56932C9.21398 7.92466 10.0883 7.5625 11 7.5625C11.9117 7.5625 12.786 7.92466 13.4307 8.56932C14.0753 9.21398 14.4375 10.0883 14.4375 11Z" fill="white"/>
+                                <path d="M0 11C0 11 4.125 3.4375 11 3.4375C17.875 3.4375 22 11 22 11C22 11 17.875 18.5625 11 18.5625C4.125 18.5625 0 11 0 11ZM11 15.8125C12.2764 15.8125 13.5004 15.3055 14.403 14.403C15.3055 13.5004 15.8125 12.2764 15.8125 11C15.8125 9.72365 15.3055 8.49957 14.403 7.59705C13.5004 6.69453 12.2764 6.1875 11 6.1875C9.72365 6.1875 8.49957 6.69453 7.59705 7.59705C6.69453 8.49957 6.1875 9.72365 6.1875 11C6.1875 12.2764 6.69453 13.5004 7.59705 14.403C8.49957 15.3055 9.72365 15.8125 11 15.8125Z" fill="white"/>
+                            </svg>								
+                        </div>
+                        <span>Learn More</span>
+                    </button>
+                </div>
+            </div>
+        <img src="src/routes/img/AI.png" alt="brain" class="w-2/4 h-2/4 mx-auto mt-10 right-0">
+        </div>
+    </div>
+    <!-- Our Team -->
+    
+    <div class="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+        <section class="w-full">
+            <h2 id="clients" class="secondary-title">Our Team</h2>
+            <p class="section-paragraph">I’ve had the pleasure of working with multiple Fortune 500 companies, designing and implementing both frontend and backend.</p>
+
+            <!-- Members -->
+            <div class="space-y-12 my-16">
+                <div class="w-full border rounded-full border-nav p-16 lg:px-32 lg:py-20 lg:space-x-32 flex justify-center flex-wrap lg:flex-nowrap">
+                    
+                    <!-- Member image -->
+                    <div class="self-center mb-6 lg:mb-0">
+                        <img class="self-center max-w-fit w-72 h-72 rounded-full" src="src/routes/img/nghia.png" alt="ayo">
+                    </div>
+
+                    <!-- Member info -->
+                    <div class="flex flex-wrap justify-center text-center lg:text-left lg:block">
+                        <h3 class="text-white text-3xl font-semibold">Nguyen Chi Nghia</h3>
+
+                        <div class="w-full lg:w-auto flex flex-wrap justify-center lg:justify-start gap-3 mt-6 mb-8">
+                            <div class="badge">Mr. CEO</div>
+                            <div class="badge">Machine Learning Development</div>
+                        </div>
+
+                        <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus, alias molestias nisi ab nesciunt dignissimos illo expedita necessitatibus nihil itaque, nostrum porro quod quae voluptatum ea rerum iure sunt aperiam totam perferendis debitis illum accusantium et. Libero quibusdam ea iusto vero sint aspernatur cumque natus illo delectus, dolores est, optio sed velit fuga, earum quam aliquid repellendus. Eligendi fugiat praesentium dolorem nobis velit voluptate error vitae minima quas itaque?</p>
+                    </div>
+
+                </div>
+                <div class="w-full border rounded-lg border-nav p-16 lg:px-32 lg:py-20 lg:space-x-32 flex justify-center lg:justify-start flex-wrap lg:flex-nowrap">
+                
+                    <!-- Member image -->
+                    <div class="mb-6 lg:mb-0">
+                        <img class="max-w-fit w-72 h-72  rounded-full" src="src/routes/img/kiet.png" alt="ayo">
+                    </div>
+
+                    <!-- Member Info -->
+                    <div class="flex flex-wrap justify-center text-center lg:text-left lg:block ">
+                        <h3 class="text-white text-3xl font-semibold">Luong Tuan Kiet</h3>
+
+                        <div class="w-full lg:w-auto flex flex-wrap justify-center lg:justify-start gap-3 mt-6 mb-8">
+                            <div class="badge">UI/UX</div>
+                            <div class="badge">Front-End Development</div>
+                        </div>
+
+                        <p class="text-secondary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus soluta velit rerum ut nobis ea optio deserunt rem obcaecati voluptatibus laboriosam temporibus sequi tempora, delectus excepturi voluptas pariatur quaerat consectetur inventore distinctio doloremque hic! Nisi, dolor. Reprehenderit natus minima ad nam repellat explicabo amet libero officia dignissimos eum consequuntur, aliquid, reiciendis omnis accusantium suscipit et, sed quam adipisci! Dolor eligendi tempore porro ut. Eius fuga numquam non hic ut laboriosam.</p>
+                    </div>
+
+                </div>
+                <div class="w-full border rounded-full  border-nav  p-16 lg:px-32 lg:py-20 lg:space-x-32 flex justify-center lg:justify-start flex-wrap lg:flex-nowrap">
+                    
+                    <!-- Member Image -->
+                    <div class="mb-6 lg:mb-0">
+                        <img class="max-w-fit w-72 h-72  rounded-full" src="src/routes/img/nhan.png" alt="ayo">
+                    </div>
+
+                    <!-- Member Logo -->
+                    <div class="flex flex-wrap justify-center text-center lg:text-left lg:block">
+                        <h3 class="text-white text-3xl font-semibold">Phan Nguyen Viet Nhan</h3>
+
+                        <div class="w-full lg:w-auto flex flex-wrap justify-center lg:justify-start gap-3 mt-6 mb-8">
+                            <div class="badge">Human Resources</div>
+                            <div class="badge">Data Analytics</div>
+                        </div>
+
+                        <p class="text-secondary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde delectus cumque, fuga sequi suscipit accusamus cum quaerat ad? Praesentium doloribus minus assumenda iure repudiandae saepe nulla tempore obcaecati laboriosam quidem, blanditiis adipisci magnam minima, suscipit neque reprehenderit beatae libero dicta tenetur a repellendus dolorem incidunt. Magni soluta quibusdam perspiciatis corrupti esse saepe veritatis est, quo, ducimus aut, vel aliquid! Molestias, fuga ipsa? Est vel provident molestiae architecto officiis dicta corrupti?</p>
+                    </div>
+
+                </div>
+                <div class="w-full border rounded-lg border-nav p-16 lg:px-32 lg:py-20 lg:space-x-32 flex justify-center lg:justify-start flex-wrap lg:flex-nowrap">
+                    
+                    <!-- Member image -->
+                    <div class="mb-6 lg:mb-0">
+                        <img class="max-w-fit w-72 h-72  rounded-full"  src="src/routes/img/minh.png" alt="ayo">
+                    </div>
+
+                    <!-- Member info -->
+                    <div class="flex flex-wrap justify-center text-center lg:text-left lg:block">
+                        <h3 class="text-white text-3xl font-semibold">Tran Nhat Minh</h3>
+
+                        <div class="w-full lg:w-auto flex flex-wrap justify-center lg:justify-start gap-3 mt-6 mb-8">
+                            <div class="badge">Data Visualization</div>
+                            <div class="badge">Business Intelligence</div>
+                        </div>
+
+                        <p class="text-secondary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo deserunt omnis aliquid, dolor expedita laborum doloremque velit quaerat perspiciatis asperiores aspernatur suscipit, illum aut fugit soluta excepturi pariatur repellendus dolores non incidunt temporibus error. Corporis architecto sint nihil, nam at ducimus earum! Facilis error quidem rerum quo dignissimos doloribus non fugiat unde fuga, corporis modi hic exercitationem adipisci labore similique laboriosam quisquam impedit recusandae dolorem autem odit saepe natus maxime.</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    </div>
+</body>
+
