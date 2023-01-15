54a2e2c773df097f1fdfa4fae693d3506b8f38e2
Mr. Nikipon <temmmyofficial@gmail.com>
2023-01-15 12:57:14 +0700

    build subtree commit 7

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			},
 			// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
 			// For example, instead of '_app', use 'app_', 'internal', etc.
-			appDir: 'internal',
+			appDir: '_app',
 		}),
 	},
 	preprocess: vitePreprocess(),
