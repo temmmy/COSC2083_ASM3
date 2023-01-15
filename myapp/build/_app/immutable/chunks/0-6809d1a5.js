677af2a41e4727cda45b82d68ad800c674c0908b
Mr. Nikipon <temmmyofficial@gmail.com>
2023-01-15 11:51:04 +0700

    build subtree commit

---
+++ myapp/build/_app/immutable/chunks/0-6809d1a5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-85a3580b.js";export{t as component};
---
+++ myapp/build/_app/immutable/chunks/1-7068e327.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-5c9349ac.js";export{t as component};
