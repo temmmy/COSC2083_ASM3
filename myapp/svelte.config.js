db96a8ba0fd60b79887992c7fa3c09859f24a1b9
Mr. Nikipon <temmmyofficial@gmail.com>
2023-01-15 12:41:59 +0700

    build subtree commit 4

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -1,16 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
-			fallback: null,
+			fallback: '200.html',
 			precompress: false,
 			strict: true,
-			fallback: 'index.html',
+			paths: {
+				base: dev ? '' : '/IntroToIT',
+			},
+			// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+			// For example, instead of '_app', use 'app_', 'internal', etc.
+			appDir: 'internal',
 		}),
 	},
 	preprocess: vitePreprocess(),
